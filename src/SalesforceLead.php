67ace426a3e38c0ddbdc828486cbfccd0b81e1b5
Ashok Ladumor <ashok@192.168.1.11>
2024-03-11 21:39:49 +0530

    Remove old files

---
+++ src/SalesforceLead.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ashokmladumor\SalesforceLead;
+use craft\base\Plugin;
+
+class SalesforceLead extends Plugin
+{
+    // Public Methods
+    // =========================================================================
+
+    public function init()
+    {
+        parent::init();
+
+        echo "YES";
+        die('------');
+    }
+}
